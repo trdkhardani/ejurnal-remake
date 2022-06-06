d0ccaef300503dea1a004821d6cf1839b2efacb3
Midyanisa Yuniar <mnynoname14@gmail.com>
2022-06-06 16:21:23 +0700

    add reviewAssignmentsModel

---
+++ app/Models/ReviewAssignmentsModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ReviewAssignmentsModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'review_assignments';
+    protected $primaryKey       = 'review_assignment_id';
+    protected $useAutoIncrement = true;
+    // protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['review_assignment_id', 'article_id', 'response', 'recommendation', 'reviewer_id', 'date_submit'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = '';
+    protected $updatedField  = '';
+    protected $deletedField  = '';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
